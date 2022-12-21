f76d8bb11d5079047c1d4633d7f510a34484d4d4
jay311311 <jay311311@gmail.com>
2022-12-21 13:21:18 +0900

    1. basic TDD (add, multiple, divide)

---
+++ TDD_Example/MathStuff.swift
@@ -0,0 +1,24 @@
+//
+//  MathStuff.swift
+//  TDD_Example
+//
+//  Created by Jooeun Kim on 2022/12/21.
+//
+
+import Foundation
+
+class MathStuff {
+    
+    func addNumber(x: Int, y:Int) -> Int {
+        return x+y
+    }
+    
+    func multipleNumber(x: Int, y:Int) -> Int {
+        return x*y
+    }
+    
+    func divideNumber(x: Int, y:Int) -> Int {
+        return x/y
+    }
+    
+}
